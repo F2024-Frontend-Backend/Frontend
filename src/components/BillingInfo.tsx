3f8189cd59d4d2a0fb7e3ee7bd22d4dbd16353ac
Bayan <113907779+BayanDowairi@users.noreply.github.com>
2024-02-15 21:48:34 +0100

    creating first name form. removing app

---
+++ src/components/BillingInfo.tsx
@@ -0,0 +1,10 @@
+const BillingInfo = () => {
+  return (
+    <form>
+      <label htmlFor="fname">First name:</label>
+      <input type="text" id="fname" name="fname" />
+    </form>
+  );
+};
+
+export default BillingInfo;

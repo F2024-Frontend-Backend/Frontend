c7ee6de1136ab1040c0c85f4d3ada3956650acda
sand4570 <s224323@student.dtu.dk>
2024-02-21 15:12:02 +0100

    create total price section

--- src/components/purchaseTotal/purchaseTotal.tsx
+++ src/components/purchaseTotal/purchaseTotal.tsx
@@ -2,13 +2,43 @@ import React from 'react'
 import './purchaseTotal.css'
 
 function PurchaseTotal () {
+    const purchasePrise = 400;
+    const deliveryPrice = 39;
+    const currency = "kr."
+
+
     return(
     <>
-        <div className='top'>
+        <span>Ordre #1234</span>
+        <div className='itemList'>
             
         </div>
-        <div className='bottom'>
 
+        <div className='totalPrice'>
+            <div className='colStart'>
+                <span className='bold'>Subtotal</span>
+                <div>
+                <span className='bold'>Taxes </span>
+                <span>(included)</span>
+                </div>
+                <span className='bold'>Delivery</span>
+            </div>
+            <div className='colEnd'>
+                <span>{purchasePrise} {currency}</span>
+                <span>{purchasePrise*0.2} {currency}</span>
+                <span>{deliveryPrice} {currency}</span>
+            </div>
+        </div>
+
+        <div className='deviderLine'></div>
+
+        <div className='totalPrice'>
+            <div className='colStart'>
+                <span className='bold'>Total</span>
+            </div>
+            <div className='colEnd'>
+                <span>{purchasePrise+deliveryPrice} {currency}</span>
+            </div>
         </div>
     </>
     )

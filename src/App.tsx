f129328e70b9d80ae881f902ee7cc190156a4fc0
sand4570 <s224323@student.dtu.dk>
2024-02-23 22:05:04 +0100

    bug fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import "./App.css";
@@ -7,17 +6,14 @@ import Basket from "./pages/Basket";
 import Checkout from "./pages/Checkout";
 import Receipt from "./pages/Receipt";
 
-
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          
           <Route path="receipt" element={<Receipt />} />
           <Route path="checkout" element={<Checkout />} />
           <Route path="" element={<Basket />} />
-      
         </Routes>
       </BrowserRouter>
     </>
@@ -25,4 +21,3 @@ function App() {
 }
 
 export default App;
-

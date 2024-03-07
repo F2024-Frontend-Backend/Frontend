b3f7dc8663ef0bffcefb072a3d9cb69658ffadf3
NegarOstad <s224283@student.dtu.dk>
2024-03-07 22:23:02 +0100

    clean test

--- src/components/Basket/BasketComponents.test.tsx
+++ src/components/Basket/BasketComponents.test.tsx
@@ -1,16 +1,3 @@
-/*
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { BasketComponent } from './BasketComponent';
-import {useBasketState} from './useBasketState'
-import { ItemDetails } from './BasketItem';
-/*
-discribe('BasketComponent', () =>{
-    test ("Remove items" , () => {
-        const items = [{id: 1, name: 'Item 1', price: 10} , {id: 2, name: 'Item 2', price: 20}]
-        render(<ItemDetails {items}/>)
-    })
-})*/
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { ItemDetails } from "./BasketItem";
